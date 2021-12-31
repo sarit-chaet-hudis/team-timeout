f08ac7cd42fe27ce66de0b9f61ed9a93a05711bf
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-31 18:59:21 +0200

    game 99% working

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -13,6 +13,8 @@ const blocks = [
 
 function GameBoard(props) {
   const [currentBlocks, setCurrentBlocks] = useState([]);
+  const [blockBeingDragged, setBlockBeingDragged] = useState(null);
+  const [blockBeingReplaced, setBlockBeingReplaced] = useState(null);
 
   const generateRandomBoard = () => {
     const randomBoard = [];
@@ -27,68 +29,69 @@ function GameBoard(props) {
     generateRandomBoard();
   }, []);
 
+  const chechRowMatch = (matchLength) => {
+    const invalid = [
+      6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+    ];
+    for (let i = 0; i < width * width; i++) {
+      // if (i === width * (i + 1) - matchLength + 1) {
+      //   // we dont need to check since we are "too close" to the end of line
+      //   // to find a row of length
+      //   i = width * (i + 1);
+      // } // TODO rewrite to not be hard coded!!!
+      if (invalid.includes(i)) continue;
+      const row = [];
+      for (let j = 0; j < matchLength; j++) {
+        // create the column we are checking, in selected length
+        row.push(i + j);
+      }
+      //console.log(row);
+      const selectedColor = currentBlocks[i];
+      if (row.every((block) => currentBlocks[block] === selectedColor)) {
+        row.forEach((block) => (currentBlocks[block] = "black"));
+        return true;
+      }
+    }
+  };
+
+  const checkColMatch = (matchLength) => {
+    const max = width * (width - matchLength + 1);
+    // we don't need to check after max since its too "low" on the board
+    // to find a column of length
+    for (let i = 0; i <= max; i++) {
+      const column = [];
+      for (let j = 0; j < matchLength; j++) {
+        // create the column we are checking, in selected length
+        column.push(i + width * j);
+      }
+      const selectedColor = currentBlocks[i];
+      if (column.every((block) => currentBlocks[block] === selectedColor)) {
+        column.forEach((block) => (currentBlocks[block] = "black"));
+        return true;
+      }
+    }
+  };
+
+  const moveIntoSquareBelow = () => {
+    const firstRow = Array.from(Array(width).keys());
+
+    for (let i = 0; i < width * (width - 1); i++) {
+      // check each row but not last one
+      if (firstRow.includes(i) && currentBlocks[i] === "black") {
+        // generate random block to fill empty block in top row
+
+        const randomBlockNo = Math.floor(Math.random() * blocks.length);
+
+        currentBlocks[i] = blocks[randomBlockNo];
+      }
+
+      if (currentBlocks[i + width] === "black") {
+        currentBlocks[i + width] = currentBlocks[i];
+        currentBlocks[i] = "black";
+      }
+    }
+  };
   useEffect(() => {
-    const chechRowMatch = (matchLength) => {
-      const invalid = [
-        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
-      ];
-      for (let i = 0; i < width * width; i++) {
-        // if (i === width * (i + 1) - matchLength + 1) {
-        //   // we dont need to check since we are "too close" to the end of line
-        //   // to find a row of length
-        //   i = width * (i + 1);
-        // } // TODO rewrite to not be hard coded!!!
-        if (invalid.includes(i)) continue;
-        const row = [];
-        for (let j = 0; j < matchLength; j++) {
-          // create the column we are checking, in selected length
-          row.push(i + j);
-        }
-        //console.log(row);
-        const selectedColor = currentBlocks[i];
-        if (row.every((block) => currentBlocks[block] === selectedColor)) {
-          row.forEach((block) => (currentBlocks[block] = "black"));
-        }
-      }
-    };
-
-    const checkColMatch = (matchLength) => {
-      const max = width * (width - matchLength + 1);
-      // we don't need to check after max since its too "low" on the board
-      // to find a column of length
-      for (let i = 0; i < max; i++) {
-        const column = [];
-        for (let j = 0; j < matchLength; j++) {
-          // create the column we are checking, in selected length
-          column.push(i + width * j);
-        }
-        const selectedColor = currentBlocks[i];
-        if (column.every((block) => currentBlocks[block] === selectedColor)) {
-          column.forEach((block) => (currentBlocks[block] = "black"));
-        }
-      }
-    };
-
-    const moveIntoSquareBelow = () => {
-      const firstRow = Array.from(Array(width).keys());
-
-      for (let i = 0; i < width * (width - 1); i++) {
-        // check each row but not last one
-        if (firstRow.includes(i) && currentBlocks[i] === "black") {
-          // generate random block to fill empty block in top row
-          console.log(`in first row and found black`);
-          const randomBlockNo = Math.floor(Math.random() * blocks.length);
-          console.log(`blocks[randomBlockNo] is ${blocks[randomBlockNo]}`);
-          currentBlocks[i] = blocks[randomBlockNo];
-        }
-
-        if (currentBlocks[i + width] === "black") {
-          currentBlocks[i + width] = currentBlocks[i];
-          currentBlocks[i] = "black";
-        }
-      }
-    };
-
     const timer = setInterval(() => {
       checkColMatch(3);
       checkColMatch(4);
@@ -100,12 +103,81 @@ function GameBoard(props) {
       setCurrentBlocks([...currentBlocks]);
     }, 100);
     return () => clearInterval(timer);
-  }, [currentBlocks]);
+  }, [currentBlocks, blockBeingDragged, blockBeingReplaced]); // eslint-disable-line
+
+  const onDragStart = (e) => {
+    console.log(e.target);
+    setBlockBeingDragged(e.target);
+  };
+  const onDrop = (e) => {
+    setBlockBeingReplaced(e.target);
+  };
+
+  const onDragEnd = () => {
+    // check if drag is valid, if so- replace blocks
+    const blockBeingDraggedId = +blockBeingDragged.getAttribute("data-id");
+
+    const blockBeingReplacedId = +blockBeingReplaced.getAttribute("data-id");
+
+    const temp = currentBlocks[blockBeingReplacedId];
+    currentBlocks[blockBeingReplacedId] = currentBlocks[blockBeingDraggedId];
+
+    currentBlocks[blockBeingDraggedId] = temp;
+
+    const validMoves = [
+      blockBeingDraggedId + 1,
+      blockBeingDraggedId - 1,
+      blockBeingDraggedId + width,
+      blockBeingDraggedId - width,
+      // TODO this does not test borders!!!
+    ];
+
+    const isValidMove = validMoves.includes(blockBeingReplacedId);
+
+    const row5 = chechRowMatch(5);
+    console.log(`row of 5? ${row5}`);
+    const row4 = chechRowMatch(4);
+    console.log(`row of 4? ${row4}`);
+    const row3 = chechRowMatch(3);
+    console.log(`row of 3? ${row3}`);
+    const col5 = checkColMatch(5);
+    console.log(`col of 5? ${col5}`);
+    const col4 = checkColMatch(4);
+    console.log(`col of 4? ${col4}`);
+    const col3 = checkColMatch(3);
+    console.log(`col of 3? ${col3}`);
+
+    if (
+      blockBeingReplacedId &&
+      isValidMove &&
+      (row3 || row4 || row5 || col3 || col4 || col5)
+    ) {
+      setBlockBeingDragged(null);
+      setBlockBeingReplaced(null);
+    } else {
+      currentBlocks[blockBeingReplacedId] =
+        blockBeingReplaced.style.backgroundColor;
+      currentBlocks[blockBeingDraggedId] =
+        blockBeingDragged.style.backgroundColor;
+      setCurrentBlocks([...currentBlocks]);
+    }
+  };
 
   return (
     <div className="board">
       {currentBlocks.map((color, index) => (
-        <div key={index} style={{ backgroundColor: color }}></div>
+        <div
+          key={index}
+          style={{ backgroundColor: color }}
+          data-id={index}
+          draggable
+          onDragStart={onDragStart}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={(e) => e.preventDefault()}
+          onDragLeave={(e) => e.preventDefault()}
+          onDrop={onDrop}
+          onDragEnd={onDragEnd}
+        ></div>
       ))}
     </div>
   );

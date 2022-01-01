d7b16c547f1d2bd86ab17afe085c9e2bd37f5223
sarit-chaet-hudis <sarit.chh@gmail.com>
2022-01-01 17:55:43 +0200

    refactor

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,5 +1,25 @@
-const Timer = () => {
-  return <div className="Timer">00:56</div>;
+import { useState, useEffect } from "react";
+
+const Timer = ({ stopGame }) => {
+  const [time, setTime] = useState(10); //TODO restart on "new Game"
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      if (time > 0) {
+        setTime(time - 1);
+        if (time === 1) {
+          clearInterval(timerInterval);
+          stopGame();
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  });
+
+  return <div className="Timer">{time}</div>;
 };
 
 export default Timer;
--- src/Pages/PlayGame.jsx
+++ src/Pages/PlayGame.jsx
@@ -1,13 +1,24 @@
 import GameBoard from "../Components/GameBoard";
-import GameControls from "../Components/GameControls";
+import Timer from "../Components/Timer";
+import Score from "../Components/Score";
 
-function Game(props) {
+function PlayGame(props) {
+  const saveScore = (scoreOnGameStop) => {
+    console.log(`you got ${scoreOnGameStop} points.`);
+  };
+
+  const stopGame = () => {
+    console.log("GAME WAS STOPPED");
+  };
+
   return (
     <div className="GamePage">
       <GameBoard />
-      <GameControls />
+      <Score saveScore={saveScore} />
+      <Timer stopGame={stopGame} />
+      <button>New Game</button>
     </div>
   );
 }
 
-export default Game;
+export default PlayGame;
